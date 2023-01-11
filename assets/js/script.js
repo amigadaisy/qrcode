150e90971d8d0584d73ac510eb5302dd11dfa17f
amigadaisy <phanlekhahan152@gmail.com>
2023-01-11 20:37:35 +0700

    #finish update
    fully update JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,8 @@ const qr = document.getElementById('qrcode');
 const onGenerateSubmit = (e) => {
   e.preventDefault();
 
+  clearUI();
+
   const url = document.getElementById('url').value;
   const size = document.getElementById('size').value;
 
@@ -11,7 +13,26 @@ const onGenerateSubmit = (e) => {
     alert('Please enter a URL');
   }  else {
     showSpinner();
+
+    setTimeout(() => {
+      hideSpinner();
+
+      generateQRCode(url, size);
+
+      setTimeout(() => {
+        const saveUrl = qr.querySelector('img').src;
+        createSaveBtn(saveUrl);
+      },50);
+    }, 1000);
   }
+};
+
+const generateQRCode = (url, size) => {
+  const qrcode = new QRCode('qrcode', {
+    text: url,
+    width: size,
+    height: size,
+  });
 };
 
 const showSpinner = () => {
@@ -20,6 +41,22 @@ const showSpinner = () => {
 
 const hideSpinner = () => {
   document.getElementById('spinner').style.display = 'none';
+};
+
+const clearUI = () => {
+  qr.innerHTML = '';
+  const saveLink = document.getElementById('save-link');
+  if (saveLink) saveLink.remove();
+};
+
+const createSaveBtn = (saveUrl) => {
+  const link = document.createElement('a');
+  link.id = 'save-link';
+  link.classList = 'bg-red-500 hover:bg-red-700 text-white font-bold py-2 rounded w-1/3 m-auto my-5';
+  link.href = saveUrl;
+  link.download ='qrcode';
+  link.innerHTML = 'save Image';
+  document.getElementById('generated').appendChild(link);
 };
 
 hideSpinner();
